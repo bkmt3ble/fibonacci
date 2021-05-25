bb0c5a339b9387f6ab60b0b3f5811ed83a78235b
Yumiko Motoyama <yumicchi_shindobat@yahoo.co.jp>
2021-05-25 22:11:02 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 'use strict';
-function fib(n){
-    if (n===0){
-        return 0;
-    }else if (n===1){
-        return 1;
-    }
-    return fib(n-1)+fib(n-2);
-}
-for (let i=0;i<20;i++){
+const memo = new Map();
 
+//決まっているものなんで最初から入れておく
+memo.set(0,0);
+memo.set(1,1);
+
+function fib(n){
+    
+    //メモにデータがあればそれを使う
+    if (memo.has(n)){
+        return memo.get(n);
+    }else   {
+        //メモにデータがないので計算する
+        let value=fib(n-1)+fib(n-2);
+        memo.set(n,value)
+        return value;
+    }
+}
+for (let i =0;i<=40;i++){
     console.log(i+"のフィボナッチ数は"+fib(i)+"です");
 }
